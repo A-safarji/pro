9f45d77c6367ced039f5151b17ba11ce113b7122
Abdultawwab Safarji <a.m.safarji@gmail.com>
2022-04-09 14:35:23 +0300

    Update scripts.js

--- social/js/scripts.js
+++ social/js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-    * -Creative v6.0.3 (https://startbootstrap.com/themes/creative)
+    *Creative v6.0.3 (https://startbootstrap.com/themes/creative)
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
     */
